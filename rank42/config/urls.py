6d260c69cec1d18e2d5918a43c80e75e230fecdb
progresshans <progresshans@gmail.com>
2020-05-10 20:16:09 +0900

    rank42 폴더 이름을 config로 변경

--- rank42/config/urls.py
+++ rank42/config/urls.py
@@ -1,4 +1,4 @@
-"""rank42 URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- rank42/main/models.py
+++ rank42/main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class FtUser(models.Model):
+	id = models.
