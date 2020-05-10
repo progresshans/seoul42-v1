379f9b616d216bd4e56d451d19fe189d37f84ff4
progresshans <progresshans@gmail.com>
2020-05-10 22:20:57 +0900

    model 작성

--- rank42/main/models.py
+++ rank42/main/models.py
@@ -1,5 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Coalition(models.Model):
+	id = models.IntegerField(unique=True, primary_key=True)
+	name = models.CharField(verbose_name="길드 이름", max_length=50)
+	color = models.CharField(verbose_name="길드 색상", max_length=7)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
 class FtUser(models.Model):
-	id = models.
+	id = models.IntegerField(unique=True, primary_key=True)
+	login = models.CharField(verbose_name="로그인 아이디", max_length=20)
+	is_alive = models.BooleanField(verbose_name="생존여부")
+	coalition = models.ForeignKey(Coalition, verbose_name="길드", on_delete=models.CASCADE())
+	coalition_point = models.IntegerField(verbose_name="길드 포인트")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+class UpdateBranch(models.Model):
+	id = models.AutoField(primary_key=True)
+	is_updating = models.BooleanField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+class PointLog(models.Model):
+	id = models.AutoField(primary_key=True)
+	ft_user = models.ForeignKey(FtUser, on_delete=models.CASCADE())
+	update_branch = models.ForeignKey(UpdateBranch, on_delete=models.CASCADE())
+	coalition_point = models.IntegerField(verbose_name="길드 포인트")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
