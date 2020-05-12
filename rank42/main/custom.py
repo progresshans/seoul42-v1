c5035b1f111f5d40221d46f614c42688c68dde92
progresshans <progresshans@gmail.com>
2020-05-12 18:16:28 +0900

    뷰와 관련있는 클래스들 커스텀으로 이동

--- rank42/main/custom.py
+++ rank42/main/custom.py
@@ -1,3 +1,9 @@
+import requests
+from django.conf import settings
+from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
 def count_page(number):
 	if int(number) <= 100:
 		return 1
@@ -5,6 +11,40 @@ def count_page(number):
 	page = page + 1 if number % (100 * page) != 0 else page
 	return page
 
+
+class FtApi:
+	ft_access_token = None
+	ft_oauth_url = "https://api.intra.42.fr/oauth/token"
+	ft_api_url = "https://api.intra.42.fr/v2/"
+	update_branch = None
+
+	def __init__(self, UpdateBranch=None):
+		self.ft_uid_key = settings.FT_UID_KEY
+		self.ft_secret_key = settings.FT_SECRET_KEY
+		self.update_branch = UpdateBranch if UpdateBranch else None
+
+	def get_access_token(self):
+		oauth_data = {
+			'grant_type': 'client_credentials',
+			'client_id': self.ft_uid_key,
+			'client_secret': self.ft_secret_key,
+		}
+		self.ft_access_token = requests.post(self.ft_oauth_url, data=oauth_data).json()['access_token']
+		return self.ft_access_token
+
+	def get_data(self, url, page=1, per_page=100, sort=None):
+		params = {
+			'access_token': self.get_access_token(),
+			'page': page if page else '',
+			'per_page': per_page if per_page else '',
+			'sort': sort if sort else '',
+		}
+		return requests.get(f'{self.ft_api_url}{url}', params=params).json()
+
+
+class SuperUserCheckMixin(UserPassesTestMixin, View):
+	def test_func(self):
+		return self.request.user.is_superuser
 
 class RankTier:
 	challenger_name = "Chanllenger"
--- rank42/main/views.py
+++ rank42/main/views.py
@@ -2,51 +2,15 @@ import requests, json
 
 from django.shortcuts import render
 from django.conf import settings
-from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views import View
 
-from .custom import count_page, RankTier
+from .custom import count_page, RankTier, FtApi, SuperUserCheckMixin
 from .models import FtUser, Coalition
 
 
 # Create your views here.
-class FtApi:
-	ft_access_token = None
-	ft_oauth_url = "https://api.intra.42.fr/oauth/token"
-	ft_api_url = "https://api.intra.42.fr/v2/"
-	update_branch = None
-
-	def __init__(self, UpdateBranch=None):
-		self.ft_uid_key = settings.FT_UID_KEY
-		self.ft_secret_key = settings.FT_SECRET_KEY
-		self.update_branch = UpdateBranch if UpdateBranch else None
-
-	def get_access_token(self):
-		oauth_data = {
-			'grant_type': 'client_credentials',
-			'client_id': self.ft_uid_key,
-			'client_secret': self.ft_secret_key,
-		}
-		self.ft_access_token = requests.post(self.ft_oauth_url, data=oauth_data).json()['access_token']
-		return self.ft_access_token
-
-	def get_data(self, url, page=1, per_page=100, sort=None):
-		params = {
-			'access_token': self.get_access_token(),
-			'page': page if page else '',
-			'per_page': per_page if per_page else '',
-			'sort': sort if sort else '',
-		}
-		return requests.get(f'{self.ft_api_url}{url}', params=params).json()
-
-
-class SuperUserCheckMixin(UserPassesTestMixin, View):
-	def test_func(self):
-		return self.request.user.is_superuser
-
-
 class ManagePage(SuperUserCheckMixin, TemplateView):
 	template_name = "manage_page.html"
 
