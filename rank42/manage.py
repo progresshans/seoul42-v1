71755bb2f2c8be63aafbb5b9129599ae315d39c8
progresshans <progresshans@gmail.com>
2020-05-12 21:50:08 +0900

    다시 변경

--- rank42/manage.py
+++ rank42/manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-if __name__ == '__main__':
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
     try:
         from django.core.management import execute_from_command_line
@@ -15,3 +15,7 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
