216ea72272d4bc3a2ab245cfd4447ca8929cf3e6
progresshans <progresshans@gmail.com>
2020-06-03 22:54:32 +0900

    RankTier class의 set_tier 수정

--- rank42/main/custom.py
+++ rank42/main/custom.py
@@ -3,7 +3,7 @@ from django_pandas.io import read_frame
 from django.conf import settings
 from django.views import View
 from django.contrib.auth.mixins import UserPassesTestMixin
-from .models import Tier
+from .models import Tier, FtUser
 
 
 def count_page(number):
@@ -58,17 +58,24 @@ class FtApi:
 
 class SuperUserCheckMixin(UserPassesTestMixin, View):
 	"""슈퍼 유저인지 확인하는 Mixin"""
+
 	def test_func(self):
 		return self.request.user.is_superuser
+
+
+class SingleTier:
+	def __init__(self, tier_name, tier_img):
+		self.tier_name = tier_name
+		self.tier_img = tier_img
 
 
 class RankTier:
 	"""rank42에서 사용되는 티어 시스템 생산 및 수정 등 모든 부분을 관리하는 class"""
 	challenger_name = "Challenger"
-	challenger_per = 0.1
+	challenger_per = 1.5
 	challenger_img = "https://opgg-static.akamaized.net/images/medals/challenger_1.png"
 	diamond_name = "Diamond"
-	diamond_per = 2.5
+	diamond_per = 4.2
 	diamond_img = "https://opgg-static.akamaized.net/images/medals/diamond_1.png"
 	platinum_name = "Platinum"
 	platinum_per = 8.3
@@ -77,10 +84,9 @@ class RankTier:
 	gold_per = 24
 	gold_img = "https://opgg-static.akamaized.net/images/medals/gold_1.png"
 	silver_name = "Silver"
-	silver_per = 35.2
+	silver_per = 30.2
 	silver_img = "https://opgg-static.akamaized.net/images/medals/silver_1.png"
 	bronze_name = "Bronze"
-	bronze_per = 22.3
 	bronze_img = "https://opgg-static.akamaized.net/images/medals/bronze_1.png"
 	unranked_name = "Unranked"
 	unranked_img = "https://opgg-static.akamaized.net/images/medals/default.png"
@@ -88,30 +94,50 @@ class RankTier:
 	def __init__(self, number=None):
 		"""'number(캠퍼스의 본과 참여 학생수)'에 맞게 티어별 수를 생성"""
 		if number:
+			tier_class_list = []
+
+			def split_tier(number, tier_name, tier_img, tier_class_list, flag=None):
+				if not flag:
+					numbers = []
+					numbers_append = numbers.append
+					for i in range(4, 0, -1):
+						numbers_append(int(number / i))
+						number -= numbers[-1]
+
+					tier_class_list_append = tier_class_list.append
+					for i, numbers_i in enumerate(numbers):
+						for _ in range(numbers_i):
+							single_tier = SingleTier(f"{tier_name}{i + 1}", tier_img)
+							tier_class_list_append(single_tier)
+				else:
+					tier_class_list_append = tier_class_list.append
+					for _ in range(number):
+						single_tier = SingleTier(tier_name, tier_img)
+						tier_class_list_append(single_tier)
+
 			self.rank_users_number = int(number)
 			self.challenger = round(number * (self.challenger_per / 100))
+			split_tier(self.challenger, self.challenger_name, self.challenger_img, tier_class_list, 1)
 			self.diamond = round(number * (self.diamond_per / 100))
+			split_tier(self.diamond, self.diamond_name, self.diamond_img, tier_class_list)
 			self.platinum = round(number * (self.platinum_per / 100))
+			split_tier(self.platinum, self.platinum_name, self.platinum_img, tier_class_list)
 			self.gold = round(number * (self.gold_per / 100))
+			split_tier(self.gold, self.gold_name, self.gold_img, tier_class_list)
 			self.silver = round(number * (self.silver_per / 100))
+			split_tier(self.silver, self.silver_name, self.silver_img, tier_class_list)
 			self.bronze = number - (self.challenger + self.diamond + self.platinum + self.gold + self.silver)
+			split_tier(self.bronze, self.bronze_name, self.bronze_img, tier_class_list)
+			self.tier_class_list = tier_class_list
 
-	def make_tier_list(self, ft_user=None):
-		"""
-		Tier 모델을 가지고 전체적으로 티어를 설정함
-		ft_user가 존재할 경우, 해당 유저의 점수를 다시 불러온 후 전체적으로 티어를 설정함
-
-		:param ft_user: 점수를 다시 불러오고자 하는 유저 queryset
-		"""
-		if ft_user:
-			tier = Tier.objects.get(id=ft_user.id)
-			ft_api = FtApi()
-			coalition_data = ft_api.get_data(url=f'users/{ft_user.id}/coalitions_users')
-			tier.coalition_point = coalition_data[0]["score"]
-			tier.save()
-		tiers = read_frame(Tier.objects.exclude(coalition_point=0))
-
-
+	# def update_ft_user(self, ft_user):
+	# 	tier = Tier.objects.get(id=ft_user.id)
+	# 	ft_api = FtApi()
+	# 	coalition_data = ft_api.get_data(url=f'users/{ft_user.id}/coalitions_users')
+	# 	tier.coalition_point = coalition_data[0]["score"]
+	# 	tier.save()
+	# 	ft_users = FtUser.objects.filter(is_alive=True).exclude(coalition_point=0).order_by('-coalition_point')
+	# 	return self.set_tier(ft_users)
 
 	def set_tier(self, ft_users, unrank_ft_users=None):
 		"""
@@ -123,19 +149,9 @@ class RankTier:
 		"""
 		for i, ft_user in enumerate(ft_users):
 			temp = {}
-			tier = Tier(id=ft_user.id)
-			if self.challenger >= i:
-				temp["tier_name"] = self.challenger_name
-			elif self.challenger + self.diamond >= i:
-				temp["tier_name"] = self.diamond_name
-			elif self.challenger + self.diamond + self.platinum >= i:
-				temp["tier_name"] = self.platinum_name
-			elif self.challenger + self.diamond + self.platinum + self.gold >= i:
-				temp["tier_name"] = self.gold_name
-			elif self.challenger + self.diamond + self.platinum + self.gold + self.silver >= i:
-				temp["tier_name"] = self.silver_name
-			else:
-				temp["tier_name"] = self.bronze_name
+			tier = Tier(FtUser=ft_user)
+			temp["tier_name"] = self.tier_class_list[i].tier_name
+			ft_user.tier_img = self.tier_class_list[i].tier_img
 			temp["tier_rank"] = i + 1
 			if temp["tier_rank"] != tier.tier_rank or temp["tier_name"] != tier.tier_name:
 				tier.tier_name = temp["tier_name"]
@@ -145,9 +161,9 @@ class RankTier:
 		# 점수가 존재하지 않는 본과 유저에 대해 처리할지는 선택 가능. 리턴값이 다름.
 		if unrank_ft_users:
 			for ft_user in unrank_ft_users:
-				tier = Tier(id=ft_user.id)
+				ft_user.tier_img = self.unranked_img
+				tier = Tier(FtUser=ft_user)
 				tier.tier_name = self.unranked_name
-				tier.tier_img = self.unranked_img
 				tier.tier_rank = 0
 				tier.save()
 			return ft_users, unrank_ft_users
