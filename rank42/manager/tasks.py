9064681e2fb16e35d53dea56cad7009b0dcb550d
progresshans <progresshans@gmail.com>
2020-08-20 00:00:05 +0900

    task 예외 처리

--- rank42/manager/tasks.py
+++ rank42/manager/tasks.py
@@ -11,25 +11,25 @@ def make_ft_user(page):
 		try:
 			FtUser.objects.get(id=data["id"])
 		except:
-			# try:
-			# detail_data = ft_api.get_data(url=f'users/{data["id"]}')
-			coalition_data = ft_api.get_data(url=f'users/{data["id"]}/coalitions_users')
-			if coalition_data and Coalition.objects.filter(id=int(coalition_data[0]["coalition_id"])).exists():
-				ft_user = FtUser.objects.create(
-					id=data["id"],
-					login=data["login"],
-					is_alive=True,
-					coalition=Coalition.objects.get(id=int(coalition_data[0]["coalition_id"])),
-				)
-				Tier.objects.create(
-					FtUser=ft_user,
-					coalition_point=coalition_data[0]["score"],
-				)
-			else:
-				FtUser.objects.create(
-					id=data["id"],
-					login=data["login"],
-					is_alive=False,
-				)
-			# except:
-			# 	pass
+			try:
+				# detail_data = ft_api.get_data(url=f'users/{data["id"]}')
+				coalition_data = ft_api.get_data(url=f'users/{data["id"]}/coalitions_users')
+				if coalition_data and Coalition.objects.filter(id=int(coalition_data[0]["coalition_id"])).exists():
+					ft_user = FtUser.objects.create(
+						id=data["id"],
+						login=data["login"],
+						is_alive=True,
+						coalition=Coalition.objects.get(id=int(coalition_data[0]["coalition_id"])),
+					)
+					Tier.objects.create(
+						FtUser=ft_user,
+						coalition_point=coalition_data[0]["score"],
+					)
+				else:
+					FtUser.objects.create(
+						id=data["id"],
+						login=data["login"],
+						is_alive=False,
+					)
+			except:
+				pass
